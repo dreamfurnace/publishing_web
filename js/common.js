002d479b8318cfef8edaae16fc6d55fe1292b66e
Lee <rosyl@naver.com>
2024-07-05 10:24:10 +0900

    페이지 추가 및 수정

--- js/common.js
+++ js/common.js
@@ -1,6 +1,14 @@
 // JavaScript Document
 $(document).ready(function () {
+     
+// open mypage
+$("#btn_openMenu").on("click", function () {
+     $("#sec_Menu").animate({right: "0"}, 100);
+});
 
+$("#btn_closeMenu").on("click", function () {
+     $("#sec_Menu").animate({right: "-100%"}, 100);
+});
 
 
 
