2cdd4a11c9f04c5f6e71e76e1305250cf6cde359
Lee <rosyl@naver.com>
2024-09-06 09:16:03 +0900

    css 수정

--- js/common.js
+++ js/common.js
@@ -2,14 +2,19 @@
 $(document).ready(function () {
      
 // open mypage
-$("#btn_openMenu").on("click", function () {
-     $("#sec_Menu").animate({right: "0"}, 100);
+     $("#btn_openMenu").on("click", function () {
+          $("#sec_Menu").animate({right: "0"}, 100);
+     });
+
+     $("#btn_closeMenu").on("click", function () {
+          $("#sec_Menu").animate({right: "-100%"}, 100);
+     });
+
+
 });
 
-$("#btn_closeMenu").on("click", function () {
-     $("#sec_Menu").animate({right: "-100%"}, 100);
-});
-
-
-});
-
+document.addEventListener('touchmove', function(event) {
+     if (event.scale !== undefined && event.scale !== 1) {
+         event.preventDefault();
+     }
+ }, { passive: false }); // for ios
